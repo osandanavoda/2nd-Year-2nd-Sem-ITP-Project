3a8f24c08a276ec5a2c53f7ac127e7a1db36bd13
osandanavoda <123382936+osandanavoda@users.noreply.github.com>
2023-01-28 11:39:48 +0530

    Add files via upload

---
+++ src/components/Procurepage.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import axios from "axios";
+import 'C:/SLIIT My Academic Files/2nd YEAR/2nd Semester (Y2 S2)/Modules/ITP (IT2080)/ITP Project/MERN Video Series/Part 1/New Prj File/itp pro2/Procurement/src/components/css/Procurepage.css';
+
+export default class Procurepage extends Component{
+
+    render(){
+        return(
+            <body className="Procurepage">
+            <h1>&nbsp;</h1>
+            <h3>&nbsp;</h3>
+
+
+<center>
+
+<div class="box4">
+
+<button type = "submit" class = "Supplier-button"><a href="/Listofregsupll"><font size = "5">Supplier</font></a></button>
+<button type = "submit" class = "Imports-button"><a href="/Getreq"><font size = "5">Imports</font></a></button>
+
+</div>
+</center>
+
+</body>
+        )
+}
+
+
+}
